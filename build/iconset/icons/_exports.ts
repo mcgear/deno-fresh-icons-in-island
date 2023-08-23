71612a4fd9e7c8b4a1c3d217ea2681b0253cdc66
mcgear <michael.gearhardt@gmail.com>
2023-08-23 03:15:38 -0600

    Sample issue

---
+++ build/iconset/icons/_exports.ts
@@ -0,0 +1,4 @@
+export * from "./icon.deps.ts"
+export * from "./XCircleIcon.tsx"
+export * from "./CheckCircleIcon.tsx"
+export * from "./ExclaimIcon.tsx"
---
+++ islands/Counter.tsx
@@ -0,0 +1,20 @@
+import type { Signal } from "@preact/signals";
+import { Button } from "../components/Button.tsx";
+import { ExclaimIcon } from "$fathym/atomic-icons";
+
+interface CounterProps {
+  count: Signal<number>;
+}
+
+export default function Counter(props: CounterProps) {
+  return (
+    <div class="flex gap-8 py-6">
+      <Button onClick={() => props.count.value -= 1}>
+        -1
+        <ExclaimIcon />
+      </Button>
+      <p class="text-3xl">{props.count}</p>
+      <Button onClick={() => props.count.value += 1}>+1</Button>
+    </div>
+  );
+}
